95e056f8148170d10550b40dad9d7ddff848bb25
Ian Vomero <ivomero@ymail.com>
2021-10-07 10:40:27 -0400

    added functionality for robots to attack dinosaurs and method to choose which dinosaurs to attack. Also all health points are being properly deducted

--- battlefield.py
+++ battlefield.py
@@ -29,23 +29,43 @@ class Battlefield:
         fleet.display_robots()
         target_robot = input("What robot would you like to attack?")
         for target in fleet.robots:
-            if target_robot == "Merrideth":
+            if target_robot == "Merrideth" and target.robot_name == "Merrideth":
                 target_robot == target.robot_name
                 target.health -= dinosaur.attack_power
-                print(target.health)
+                run.show_robo_opponent_options()
                 break
-            elif target_robot == "Delilah":
+            elif target_robot == "Delilah" and target.robot_name == "Delilah":
                 target_robot == target.robot_name
                 target.health -= dinosaur.attack_power
-                print(target.health)
+                run.show_robo_opponent_options()
                 break
-            elif target_robot == "BethBeth":
+            elif target_robot == "BethBeth" and target.robot_name == "BethBeth":
                 target_robot == target.robot_name
                 target.health -= dinosaur.attack_power
                 print(target.health)
+                run.show_robo_opponent_options()
+                break
 
     def robo_turn(self, robot):
-        pass
+        herd.display_dinosaurs()
+        target_dinosaur = input("What dinosaur would you like to attack?")
+        for target in herd.dinosaurs:
+            if target_dinosaur == "Ken" and target.dinosaur_name == "Ken":
+                target_dinosaur == target.dinosaur_name
+                target.health -= robot.weapon.attack_power
+                run.show_dino_opponent_options()
+                break
+            elif target_dinosaur == "Dennis" and target.dinosaur_name == "Dennis":
+                target_dinosaur == target.dinosaur_name
+                target.health -= robot.weapon.attack_power
+                run.show_dino_opponent_options()
+                break
+            elif target_dinosaur == "Oscar" and target.dinosaur_name == "Oscar":
+                target_dinosaur == target.robot_name
+                target.health -= robot.weapon.attack_power
+                print(target.health)
+                run.show_dino_opponent_options()
+                break
 
     def show_dino_opponent_options(self):
         fleet.display_robots()
@@ -53,26 +73,41 @@ class Battlefield:
         chosen_dinosaur = input("choose dinosaur")
         for name in herd.dinosaurs:
             if chosen_dinosaur == "Ken" and name.dinosaur_name == "Ken":
-                chosen_dinosaur == name.dinosaur_name
-                print(name.dinosaur_name)
+
                 print("Robots")
                 run.dino_turn(name)
                 break
             elif chosen_dinosaur == "Dennis" and name.dinosaur_name == "Dennis":
-                chosen_dinosaur == name.dinosaur_name
-                print(name.dinosaur_name)
+
                 print("Robots")
                 run.dino_turn(name)
                 break
             elif chosen_dinosaur == "Oscar" and name.dinosaur_name == "Oscar":
-                chosen_dinosaur == name.dinosaur_name
-                print(name.dinosaur_name)
+
                 print("Robots")
                 run.dino_turn(name)
                 break
 
     def show_robo_opponent_options(self):
-        pass
+        fleet.display_robots()
+        herd.display_dinosaurs()
+        chosen_robot = input("choose robot")
+        for name in fleet.robots:
+            if chosen_robot == "Merrideth" and name.robot_name == "Merrideth":
+
+                print("Dinosaurs")
+                run.robo_turn(name)
+                break
+            elif chosen_robot == "Delilah" and name.robot_name == "Delilah":
+
+                print("Dinosaurs")
+                run.robo_turn(name)
+                break
+            elif chosen_robot == "BethBeth" and name.robot_name == "BethBeth":
+
+                print("Dinosaurs")
+                run.robo_turn(name)
+                break
 
     def display_winner(self):
         pass
